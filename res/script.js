496bb473749e2ceaa49f26d937008b30428a2ed0
kirill.kram <kkramarskiy@gmail.com>
2020-09-23 11:44:21 +0400

    some more tests

--- res/script.js
+++ res/script.js
@@ -1,3 +1,12 @@
 "use strict";
 
-alert("Hello visitor!")
+// function pow(a=prompt('a=', 0), n=prompt('n=', 0)){
+//     let res = 1;
+//     for(let i=0;i<n;i++){
+//             res *= a;
+//     }
+//     return res;
+// }
+// alert(pow())
+
+// alert('На русском')
